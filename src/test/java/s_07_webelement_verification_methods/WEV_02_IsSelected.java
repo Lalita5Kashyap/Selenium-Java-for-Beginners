2d225d64c4acfa0c0f6ee3621a3e94f8f2af6fd2
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-04 16:00:27 +0530

    push changes

--- src/test/java/s_07_webelement_verification_methods/WEV_02_IsSelected.java
+++ src/test/java/s_07_webelement_verification_methods/WEV_02_IsSelected.java
@@ -1,9 +1,18 @@
 package s_07_webelement_verification_methods;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WEV_02_IsSelected {
 
 	public static void main(String[] args) {
-		
-	}
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
 
+		WebElement poll = driver.findElement(By.xpath("//div[@id='poll-block-1']"));
+		System.out.println(poll.isSelected());
+	}
 }
--- src/test/java/s_07_webelement_verification_methods/WEV_03_IsEnabled.java
+++ src/test/java/s_07_webelement_verification_methods/WEV_03_IsEnabled.java
@@ -1,9 +1,18 @@
 package s_07_webelement_verification_methods;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WEV_03_IsEnabled {
 
 	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
 
+		WebElement voteButton = driver.findElement(By.xpath("//input[@id='vote-poll-1']"));
+		System.out.println(voteButton.isEnabled());
 	}
-
 }

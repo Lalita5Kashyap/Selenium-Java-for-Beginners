740f350bb445512c4b8a6a5462a4ab53b0e45a96
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-04 14:54:07 +0530

    push changes

--- src/test/java/s_06_WebElement_Getter_Methods/WEG_07_GetDomAttribute.java
+++ src/test/java/s_06_WebElement_Getter_Methods/WEG_07_GetDomAttribute.java
@@ -1,10 +1,32 @@
 package s_06_WebElement_Getter_Methods;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WEG_07_GetDomAttribute {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
 
+		WebElement search = driver.findElement(By.id("small-searchterms"));
+
+		System.out.println("-------Before enter any value-------");
+
+		System.out.println(search.getDomAttribute("value"));
+
+		System.out.println(search.getDomProperty("value"));
+
+		search.clear();
+		search.sendKeys("books");
+
+		System.out.println("-------After enter any value-------");
+
+		System.out.println(search.getDomAttribute("value"));
+
+		System.out.println(search.getDomProperty("value"));
 	}
-
 }

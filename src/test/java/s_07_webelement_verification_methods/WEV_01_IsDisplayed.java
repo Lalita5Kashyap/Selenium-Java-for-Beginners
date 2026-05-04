bb4124bc928ae8e9657492e9fdf147e1589bb9b1
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-04 15:30:01 +0530

    push changes

--- src/test/java/s_07_webelement_verification_methods/WEV_01_IsDisplayed.java
+++ src/test/java/s_07_webelement_verification_methods/WEV_01_IsDisplayed.java
@@ -1,10 +1,16 @@
 package s_07_webelement_verification_methods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WEV_01_IsDisplayed {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://x.com/");
+		
+		driver.get
 	}
 
 }
---
+++ src/test/java/s_07_webelement_verification_methods/WEV_02_IsSelected.java
@@ -0,0 +1,9 @@
+package s_07_webelement_verification_methods;
+
+public class WEV_02_IsSelected {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
